ac4bb2adf8ff2f659ae7851d7dc72df1edbd0f5a
Tabriz Habiyev <tabrizmonzo@gmail.com>
2022-10-02 16:41:31 +0400

    feat:

---
+++ MVC_TemplateApp/Data/MVC_TemplateAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_TemplateApp.Models;
+using System;
+
+namespace MVC_TemplateApp.Data
+{
+    public class MVC_TemplateAppContext : DbContext
+    {
+        public MVC_TemplateAppContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductPhoto> ProductPhotos { get; set; }
+    }
+}
